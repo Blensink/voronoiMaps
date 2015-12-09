f45f96cbb2f0d3d15e39de95bb00b386df27c626
Brendan Lensink <blensink192@gmail.com>
2015-12-09 00:14:06 -0800

    Figured out how to make the seed points.

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
  * Entry point for voronoi map generator.
  *
  */
+?>
 
-<script type="text/javascript" src="js/bootstrap/bootstrap.min.js"></script>
- ?>
+<canvas id='mapCanvas' width='1000' height='500' style='border:1px solid #000;'></canvas>
+
+<script type='text/javascript' src='js/generator.js'></script>
+
+<?php
+?>
